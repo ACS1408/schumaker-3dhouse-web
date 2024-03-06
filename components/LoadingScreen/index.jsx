3418a4d91a269b148a31e25a30e14a244325e811
Abhilash C S <abhilash.acs14@gmail.com>
2024-03-06 16:52:57 +0530

    loader fix

--- components/LoadingScreen/index.jsx
+++ components/LoadingScreen/index.jsx
@@ -1,4 +1,3 @@
-import { Html } from "@react-three/drei";
 import React from "react";
 import Style from "./LoadingScreen.module.scss";
 import Image from "next/image";
@@ -7,12 +6,12 @@ const LoadingScreen = () => {
   const classes = {
     loading_screen:
       "fixed w-[100vw] h-svh top-0 left-0 z-50 bg-white flex flex-col justify-center items-center",
-    loading_content: "text-center z-[2] text-white",
+    loading_content: "text-center z-[2] text-white ff-chronicle-display",
     loading_image: "absolute size-full blur-[14px] brightness-[0.6]",
     loading_text: {
       default: "",
-      line_1: "text-3xl mb-3",
-      line_2: "text-8xl",
+      line_1: "text-3xl mb-5 tracking-wider",
+      line_2: "text-8xl leading-[1.1]",
     },
     progress: "absolute bottom-0 left-0 w-full",
     progress_text: "text-center mb-2 text-white",
@@ -23,32 +22,30 @@ const LoadingScreen = () => {
     },
   };
   return (
-    // <Html>
-      <div className={classes.loading_screen}>
-        <figure className={classes.loading_image}>
-          <Image
-            src="/images/loading-screen.jpg"
-            fill
-            alt="loading-screen-image"
-            sizes="100vw"
-            quality={90}
-          />
-        </figure>
-        <div className={classes.loading_content}>
-          <div className={classes.loading_text.default}>
-            <div className={classes.loading_text.line_1}>WELCOME TO THE</div>
-            <div className={classes.loading_text.line_2}>SCHUMAKER</div>
-            <div className={classes.loading_text.line_2}>HOUSE</div>
-          </div>
-        </div>
-        <div className={classes.progress}>
-          <div className={classes.progress_text}>Loading Scene...</div>
-          <div
-            className={`${Style.progress_anim} ${classes.progress_bar.default} ${classes.progress_bar.after}`}
-          />
+    <div className={classes.loading_screen}>
+      <figure className={classes.loading_image}>
+        <Image
+          src="/images/loading-screen.jpg"
+          fill
+          alt="loading-screen-image"
+          sizes="100vw"
+          quality={90}
+        />
+      </figure>
+      <div className={classes.loading_content}>
+        <div className={classes.loading_text.default}>
+          <div className={classes.loading_text.line_1}>WELCOME TO THE</div>
+          <div className={classes.loading_text.line_2}>SCHUMACHER</div>
+          <div className={classes.loading_text.line_2}>HOUSE</div>
         </div>
       </div>
-    // </Html>
+      <div className={classes.progress}>
+        <div className={classes.progress_text}>Loading Scene...</div>
+        <div
+          className={`${Style.progress_anim} ${classes.progress_bar.default} ${classes.progress_bar.after}`}
+        />
+      </div>
+    </div>
   );
 };
 
